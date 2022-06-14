e4154406348e45abc6adfc0ab349304c41ec1670
Augustus2001 <107476202+Augustus2001@users.noreply.github.com>
2022-06-14 19:09:57 +0700

    package_pyautogui_search&&dowload_picture
    still got bug at openbrowser.py when run load() at the even order load() won't run.

--- python/openbrowser.py
+++ python/openbrowser.py
@@ -2,13 +2,12 @@
 import time
 import webbrowser
 import pyautogui
-
 """
-test open url and dowload picture
 def opensearch(a):
     webbrowser.open("www.google.com")
     pyautogui.sleep(1)
     pyautogui.click(170,490)
+    time.sleep(1)
     pyautogui.write(a+" map")
     pyautogui.press("enter")
     pyautogui.sleep(1)
@@ -22,17 +21,30 @@ def opensearch(a):
     pyautogui.sleep(1)
     pyautogui.write(a)
     pyautogui.press("enter")
+    time.sleep(1)
+    pyautogui.hotkey("ctrl","w")
 opensearch("japan")
 opensearch("thailand")
 opensearch("france")
 opensearch("itali")
+
 """
-#more easy way to dowload
-#take screen shot pyautogui.screenshot()
-url="www.google.com"
-webbrowser.open(url)
-time.sleep(1)
-pyautogui.write("thailand",interval=0.01)
-pyautogui.press("enter")
-time.sleep(1)
-pyautogui.screenshot("{}.jpg".format("thailand"))
+#more easy
+#take screen shot pyautogui.screenshot()itali
+
+def load(word):
+    url="www.google.com"
+    webbrowser.open(url)
+    time.sleep(1)
+    pyautogui.write(word)
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(2)
+    pyautogui.screenshot("{}.jpg".format(word))
+    time.sleep(1)
+    pyautogui.hotkey("ctrl","w")
+def main():
+    return
+if __name__=="__main__":
+    load("thailand")
+    load("england")
